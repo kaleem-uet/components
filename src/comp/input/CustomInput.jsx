4e4b562b580b5b0b40e3d9ee8df6a8371e163a46
kaleem-uet <kaleemullahk989@gmail.com>
2024-08-09 23:29:33 +0500

    admin added

--- src/comp/input/CustomInput.jsx
+++ src/comp/input/CustomInput.jsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import { alpha, styled } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
 import Box from "@mui/material/Box";
@@ -6,29 +5,21 @@ import InputLabel from "@mui/material/InputLabel";
 import TextField from "@mui/material/TextField";
 import FormControl from "@mui/material/FormControl";
 
-const CssTextField = styled(TextField)({
-  "& label.Mui-focused": {
-    color: "#A0AAB4",
-  },
-  "& .MuiInput-underline:after": {
-    borderBottomColor: "#B2BAC2",
-  },
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      borderColor: "#E0E3E7",
-    },
-    "&:hover fieldset": {
-      borderColor: "#B2BAC2",
-    },
-    "&.Mui-focused fieldset": {
-      borderColor: "#6F7E8C",
-    },
-  },
-});
-
 const BootstrapInput = styled(InputBase)(({ theme }) => ({
   "label + &": {
     marginTop: theme.spacing(3),
+  },
+  "& input:valid + fieldset": {
+    borderColor: "#E0E3E7",
+    borderWidth: 1,
+  },
+  "& input:invalid + fieldset": {
+    borderColor: "red",
+    borderWidth: 1,
+  },
+  "& input:valid:focus + fieldset": {
+    borderLeftWidth: 4,
+    padding: "4px !important", // override inline-style
   },
   "& .MuiInputBase-input": {
     borderRadius: 4,
@@ -38,7 +29,7 @@ const BootstrapInput = styled(InputBase)(({ theme }) => ({
     borderColor: theme.palette.mode === "light" ? "#E0E3E7" : "#2D3843",
     fontSize: 16,
     width: "100%", // Change this to 100%
-    padding: "10px 12px",
+    padding: "6px 6px",
     transition: theme.transitions.create([
       "border-color",
       "background-color",
@@ -59,31 +50,7 @@ const BootstrapInput = styled(InputBase)(({ theme }) => ({
     ].join(","),
     "&:focus": {
       boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
-      borderColor: theme.palette.primary.main,
-    },
-  },
-}));
-const RedditTextField = styled((props) => (
-  <TextField InputProps={{ disableUnderline: true }} {...props} />
-))(({ theme }) => ({
-  "& .MuiFilledInput-root": {
-    overflow: "hidden",
-    borderRadius: 4,
-    backgroundColor: theme.palette.mode === "light" ? "#F3F6F9" : "#1A2027",
-    border: "1px solid",
-    borderColor: theme.palette.mode === "light" ? "#E0E3E7" : "#2D3843",
-    transition: theme.transitions.create([
-      "border-color",
-      "background-color",
-      "box-shadow",
-    ]),
-    "&:hover": {
-      backgroundColor: "transparent",
-    },
-    "&.Mui-focused": {
-      backgroundColor: "transparent",
-      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
-      borderColor: theme.palette.primary.main,
+      borderColor: theme.palette.primary.dark,
     },
   },
 }));
@@ -100,6 +67,11 @@ const ValidationTextField = styled(TextField)({
   "& input:valid:focus + fieldset": {
     borderLeftWidth: 4,
     padding: "4px !important", // override inline-style
+  },
+
+  "& .MuiOutlinedInput-input": {
+    // height: "10px", // Adjust the height as needed
+    padding: "10px 10px",
   },
 });
 function CustomInput() {
@@ -120,7 +92,6 @@ function CustomInput() {
         <BootstrapInput id="bootstrap-input" sx={{ width: "100%" }} />
       </FormControl>
 
-      <CssTextField label="Custom CSS" id="custom-css-outlined-input" />
       <ValidationTextField
         label="CSS validation style"
         required
